c31d798013b264986e981611977b920a63041d3a
Qhama Hani <qhamahani@gmail.com>
2025-12-14 13:30:36 +0200

    Import json function

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -87,6 +87,16 @@ function exportQuotesToJSON() {
 
 exportButton.addEventListener("click", exportQuotesToJSON);
 
+function importFromJsonFile(event) {
+  const fileReader = new FileReader();
+  fileReader.onload = function (event) {
+    const importedQuotes = JSON.parse(event.target.result);
+    quotes.push(...importedQuotes);
+    saveQuotes();
+    alert("Quotes imported successfully!");
+  };
+  fileReader.readAsText(event.target.files[0]);
+}
 //Event listener for new quote button
 newQuoteButton.addEventListener("click", showRandomQuote);
 //Event listener for add quote button
